fdb288e1f17211cc725e5a4bc5d9fa5467c6b3b1
martinbarra <69089716+martinbarra@users.noreply.github.com>
2024-05-22 12:30:35 -0400

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,17 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('role');
-            $table->integer('personal_type');
+            $table->string('role')->nullable()->change();
+            $table->integer('personal_type')->nullable()->change();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
