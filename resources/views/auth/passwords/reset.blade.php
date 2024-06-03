02141fe6730414f36deba90bf729ea8871e80cbe
Rayen <rnahuelhual@ing.ucsc.cl>
2024-06-03 19:15:54 -0400

    auth

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,6 +3,67 @@
     Reset Password
 @endsection
 @section('contenido')
+
+<section class="min-h-screen flex items-center justify-center">
+    <div class="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center dark:bg-gray-900">
+        
+        <div class="px-8">
+            <h2 class="font-bold text-2xl text-[#002D74]">Establecer nueva contraseña</h2>
+            <div class="flex">
+                <p class="text-xs mt-2 font-bold  text-[#002D74]">Complete el siguiente formulario para establecer la nueva contraseña.
+                </p>
+            </div>
+            <form action="{{ route('password.email') }}" method="POST" class="flex flex-col gap-2">
+                @csrf
+                @if ($errors->any())
+                    <div class="invalid-feedback" role="alert">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <input type="hidden" name="token" value="{{ $token }}">
+                <label for="password" class="mb-2 block text-gray-700 font-bold">
+                    Contraseña
+                </label>
+                <input id="password"
+                name="password"
+                type="password"
+                placeholder="Escribe tu contraseña"
+                class="border  p-2 w-full rounded-xl
+                {{ $errors->has('password') ? ' is-invalid': '' }}" name="password" 
+                required autocomplete="new-password" 
+                >
+                <div class="invalid-feedback">
+                    {{ $errors->first('password') }}
+                </div>
+
+                <label for="password_confirmation" class="mb-2 block  text-gray-700 font-bold">
+                    Confirmar contraseña
+                </label>
+                <input id="password_confirmation"
+                       name="password_confirmation"
+                       type="password"
+                       placeholder="Repite tu contraseña"
+                       class="border p-2 w-full rounded-xl
+                       {{ $errors->has('password_confirmation') ? ' is-invalid': '' }}" name="password_confirmation" 
+                       required autocomplete="new-password" 
+                >
+                <div class="invalid-feedback">
+                    {{ $errors->first('password_confirmation') }}
+                </div>
+
+                <button type="submit" class="bg-orange-400 rounded-xl text-white py-2  hover:scale-105 duration-300">
+                    Establecer nueva contraseña
+                </button>
+            </form>
+        </div>
+    </div>
+</section>
+
+<!--
     <div class="card card-primary">
         <div class="card-header"><h4>Set a New Pasword</h4></div>
 
@@ -19,14 +80,8 @@
                     </div>
                 @endif
                 <input type="hidden" name="token" value="{{ $token }}">
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                           name="email" tabindex="1" value="{{ old('email') }}" autofocus>
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
-                    </div>
-                </div>
+
+                
                 <div class="form-group">
                     <label for="password" class="control-label">Password</label>
                     <input id="password" type="password"
@@ -56,4 +111,5 @@
     <div class="mt-5 text-muted text-center">
         Recalled your login info? <a href="{{ route('login') }}">Sign In</a>
     </div>
+-->
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,6 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.app-user')
 
-@section('content')
+@section('contenido')
+
+<section class="flex min-h-screen justify-center items-center" >
+    <div class="bg-gray-100 flex rounded-2xl shadow-xl max-w-3xl p-5 items-center">
+       <div class="px-8 w-full">
+          <h2 class="font-bold text-2xl text-[#002D74]"> Verifica tu correo electrónico</h2>
+          <div class=" py-5 w-full">
+             @if (session('resent'))
+             <div class="p-4 mb-4 text-sm border border-green-300 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                <span class="font-medium">A fresh verification link has been sent to your email address</span>
+             </div>
+             @endif
+             <p class="text-xs mt-2 font-bold  text-[#002D74]">Before proceeding, please check your email for a verification link.If you did not receive
+                the email,</p>
+                <a href="#" class="text-xs font-semibold underline hover:no-underline  text-[#002D74]">click here to request another</a>
+         </div>
+       </div>
+    </div>
+ </section>
+
+
+
+
+ <!--
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-7" style="margin-top: 2%">
@@ -21,4 +44,5 @@
             </div>
         </div>
     </div>
+-->
 @endsection
